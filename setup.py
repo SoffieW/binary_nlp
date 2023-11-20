efa00b7fff8f2381c7aaaad11dfe7e8930e01fc4
SoffieW <soffie.wis@gmail.com>
2023-11-20 11:54:45 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='building an nlp model to classify short texts as funny or not.',
+    author='Soffie',
+    license='',
+)
